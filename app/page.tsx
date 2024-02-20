160da85c9f0438a03aacf23b49d836446354902c
Nathan Dockens <ndockens92@gmail.com>
2024-02-20 17:55:05 -0600

    Style Add Goal button on home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const goals = await getGoals();
   return (
     <main className="bg-slate-900 flex flex-col items-center min-h-screen p-24">
-      <Link href="/goals/new" className="text-sky-500">Add Goal</Link>
+      <Link href="/goals/new" className="bg-sky-500 min-w-32 mt-9 p-3 rounded-xl text-center text-sky-50 hover:bg-sky-400">Add Goal</Link>
       <div className="flex flex-col m-5">
         {goals.map((x: any) => <div key={x.id} className="bg-slate-800 m-1 p-4 rounded-xl text-slate-300">{x.title}</div>)}
       </div>
