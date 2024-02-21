d9e13c8c985cecf41128651990724976f983bb82
Nathan Dockens <ndockens92@gmail.com>
2024-02-20 18:10:37 -0600

    Adjust spacing

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default async function Home() {
       <div className="font-bold text-center text-5xl">Stay <span className="text-sky-300">on Track</span>.</div>
       <div className="font-bold text-center text-5xl"><span className="text-sky-300">Crush</span> Your Goals.</div>
       <div className="font-bold text-center text-5xl">Celebrate With Your <span className="text-sky-300">Friends</span>.</div>
-      <Link href="/goals/new" className="bg-sky-500 min-w-32 mt-12 p-3 rounded-xl text-center text-sky-50 hover:bg-sky-400">Add Goal</Link>
-      <div className="flex flex-col m-5">
+      <Link href="/goals/new" className="bg-sky-500 min-w-32 mt-14 p-3 rounded-xl text-center text-sky-50 hover:bg-sky-400">Add Goal</Link>
+      <div className="flex flex-col m-8">
         {goals.map((x: any) => <div key={x.id} className="bg-slate-800 m-1 p-4 rounded-xl text-slate-300">{x.title}</div>)}
       </div>
     </main>
