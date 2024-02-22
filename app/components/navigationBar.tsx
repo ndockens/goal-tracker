48a78f3efe10d0bc92b2ba7659d98b09d282dde5
Nathan Dockens <ndockens92@gmail.com>
2024-02-22 13:32:46 -0600

    Require authentication to access goal features

---
+++ app/components/navigationBar.tsx
@@ -0,0 +1,9 @@
+import AuthButton from "./authButton";
+
+export default function NavigationBar() {
+    return (
+      <div className="bg-slate-900 flex flex-row-reverse p-4">
+        <AuthButton />
+      </div>
+    )
+}
