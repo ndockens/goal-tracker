fce852ab7e984454a5d2359bcdbbce090e77a8e1
Nathan Dockens <ndockens92@gmail.com>
2024-02-21 19:36:11 -0600

    Add user signup page

--- app/goals/new/page.tsx
+++ app/goals/new/page.tsx
@@ -23,7 +23,7 @@ export default function NewGoal() {
     <main className="bg-slate-900 flex flex-col items-center min-h-screen p-24">
       <div className="bg-slate-800 flex flex-col items-center m-10 p-7 rounded-xl text-center">
         <div className="text-lg text-slate-50">What do you want to accomplish?</div>
-        <input name="title" type="text" value={title} onChange={onTitleChange} placeholder="e.g. Win the Nobel prize" className="bg-slate-700 min-w-64 mt-4 p-3 rounded-xl text-center text-slate-300 focus:outline-none focus:ring focus:ring-slate-400"></input>
+        <input name="title" type="text" value={title} onChange={onTitleChange} placeholder="e.g. Win the Nobel prize" className="bg-slate-700 min-w-64 mt-4 p-3 rounded-xl text-center text-slate-300 focus:outline-none focus:ring focus:ring-slate-400" />
         <button className="bg-sky-500 min-w-32 mt-9 p-3 rounded-xl text-center text-sky-50 hover:bg-sky-400" onClick={onSubmit}>Submit</button>
       </div>
     </main>
